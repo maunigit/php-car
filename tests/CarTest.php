d969c2622687406104a4dc46438c8623cde0ed4b
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-24 16:53:29 +0200

    add capabilities and import

---
+++ tests/CarTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "vendor/autoload.php";
+require_once("PHPUnit/Extensions/AppiumTestCase.php");
+require_once("PHPUnit/Extensions/AppiumTestCase/Element.php");
+
+class CarTest extends PHPUnit_Extensions_AppiumTestCase {
+
+  public static $browsers = array(
+    array(
+      "local" => true,    
+      "port" => 4723,    
+      "browserName" => '',    
+      "desiredCapabilities" => array(
+        "deviceName" => "MI5",
+        "udid" => "26060fa3",
+        "platformName" => "Android",
+        "platformVersion" => "8.0.0",
+        "appPackage" => "com.carquery.app",
+        "appActivity" => "host.exp.exponent.MainActivity",
+        "automationName" => "UiAutomator2",
+        "appWaitForLaunch" => "false"   
+      )    
+    )    
+  );
+}
+?>
